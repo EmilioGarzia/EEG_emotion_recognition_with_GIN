3705a476f7cdbced0b29dbc909c77de99850c427
EmilioGarzia <74497247+EmilioGarzia@users.noreply.github.com>
2023-09-17 18:53:57 +0200

    Update confusion_matrix.py

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -4,6 +4,7 @@ import numpy as np
 import argparse   
 
 def create_confusion_matrix(filename):
+    # Data parsing
     lines = []
     with open(filename, "r") as f:
         lines = f.readlines()
@@ -14,12 +15,16 @@ def create_confusion_matrix(filename):
         data.append(row)
     data = np.transpose(data)
 
+    # F1-Score
+    f1 = metrics.f1_score(data[0],data[1], average="weighted")
+    print(f1)
+    
+    #Confusion Matrix
     confusion_matrix = metrics.confusion_matrix(data[0], data[1])
     cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=["Neutral", "Sad", "Fear", "Happy"])
     cm_display.plot()
     plt.show()
-    f1 = metrics.f1_score(data[0],data[1], average="weighted")
-    print(f1)
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", type=str, help="Path to the data")
